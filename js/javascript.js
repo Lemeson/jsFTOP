ca35beb092bff9051bada39379c217399b758fdb
Lemeson <lemeson93@gmail.com>
2025-01-30 13:35:56 -0300

    Retificação da função add7

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,9 @@
-function add7() {
-    var number = document
-    return( console.log (number + 7));
+function add7(event) {
+    if (event) {
+        event.preventDefault();
+    }
+
+    const numberInput = document.getElementById("numberInput").value;
+    const result = parseFloat(numberInput) + 7;
+    document.getElementById("result").textContent = "Result: " + result;
 }
