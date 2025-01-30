22e85526bb83ce8ecb93a28a410c302375b6b8ab
Lemeson <lemeson93@gmail.com>
2025-01-30 06:47:38 -0300

    Exercise 1 complete

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,6 @@
 function add7() {
-    var number = document
-    return( console.log (number + 7));
+    var number = document.getElementById("numberInput").value;
+    var result = parseFloat(number) + 7;
+    document.getElementById("result").innerText = result;
+    return( console.log(result));
 }
