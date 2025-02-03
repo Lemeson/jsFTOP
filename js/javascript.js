17f835e9e3da84756bc65cee7049b70cf8927608
Lemeson <lemeson93@gmail.com>
2025-02-03 06:59:39 -0300

    Exercise 2 part 2 foundations TOP complete

--- js/javascript.js
+++ js/javascript.js
@@ -4,3 +4,18 @@ function add7() {
     document.getElementById("result").innerText = result;
     return( console.log(result));
 }
+
+function multiply() {
+    var number1 = prompt("Please enter the first number: ");
+    var number2 = prompt("Please enter the second number: ");
+
+    number1 = parseFloat(number1);
+    number2 = parseFloat(number2);
+
+    result2 = number1 * number2;
+    console.log(`${number1} * ${number2} =  ${number1 * number2}`);
+
+    
+
+    document.getElementById("result2").textContent = result2
+}
