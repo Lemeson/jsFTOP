4898d1d4d85fdf499ccf39781fcbadcf2d70183d
Lemeson <lemeson93@gmail.com>
2025-02-12 17:58:23 -0300

    -

--- js/javascript.js
+++ js/javascript.js
@@ -5,16 +5,20 @@ function add7() {
 }
 
 function multiply() {
-    var number1 = prompt("Please enter the first number: ");
-    var number2 = prompt("Please enter the second number: ");
+    var number1 = document.getElementById("exercise2N1").value;
+    var number2 = document.getElementById("exercise2N2").value;
 
     number1 = parseFloat(number1);
     number2 = parseFloat(number2);
 
-    result2 = number1 * number2;
+    //var result2 = number1 * number2;
+
+    const result2 = () => number1 * number2;
+    const productResult = result2();
+
     console.log(`${number1} * ${number2} =  ${number1 * number2}`);
 
-    document.getElementById("result2").textContent = result2;
+    document.getElementById("result2").innerText = productResult;
 }
 
 function capitalize() {
